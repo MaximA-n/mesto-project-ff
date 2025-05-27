3f1524dafb7f463707c44358af18c598461d90a2
Maxim Audman <maximaudman@yandex.ru>
2025-05-27 12:52:49 +0300

    v2

--- scripts/index.js
+++ scripts/index.js
@@ -11,24 +11,26 @@
 const addButton = document.querySelector(".profile__add-button");
 const placesList = document.querySelector(".places__list");
 
-initialCards.forEach(function (card) {
-  placesList.append(moveCard(card));
+initialCards.forEach(card => {
+  placesList.append(createCard(card, deleteCard));
 });
 
-function moveCard(card) {
+function deleteCard(card) {
+  card.remove()
+}
+
+function createCard(card, deleteCard) {
   const cardTemplate = document.querySelector("#card-template").content;
   const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
   const cardImage = cardElement.querySelector(".card__image");
   const cardTitle = cardElement.querySelector(".card__description");
   const deleteButton = cardElement.querySelector(".card__delete-button");
 
-  deleteButton.addEventListener("click", function () {
-    const deleteCard = deleteButton.closest(".card");
-    deleteCard.remove();
-  });
+  deleteButton.addEventListener('click', () => deleteCard(cardElement))
 
   cardImage.src = card.link;
   cardTitle.textContent = card.name;
+  cardImage.alt = "Здесь должен быть пейзаж";
 
   return cardElement;
 }
